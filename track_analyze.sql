37d4a433807775b1a2a7cb08b4854a555602c91d
Nipphit Apisitpuwakul <nipphit.api@student.mahidol.ac.th>
2024-06-01 20:40:22 +0700

    feat: update extract_analyze

--- track_analyze.sql
+++ track_analyze.sql
@@ -38,93 +38,90 @@ MODIFY COLUMN album_href VARCHAR(100);
 
 -- begin analysis
 
---  1. top 10 track
+-- 1. top 10 track
 SELECT track_name, album_name, popularity
 FROM track_data_cleaned
 ORDER BY popularity DESC
 LIMIT 10;
 
---  2. top album and the released year
-SELECT DISTINCT album_name, album_type, album_popularity, YEAR(release_date) as released_year
-FROM track_data_cleaned
-WHERE album_type = 'album'
-ORDER BY album_popularity DESC;
+-- 2. album and the released year
+WITH ranked_track_album AS (
+    SELECT track_name, album_name, popularity, YEAR(release_date) as released_year, total_tracks, album_type,
+        ROW_NUMBER() OVER (PARTITION BY album_name ORDER BY popularity DESC) AS rank_track
+    FROM track_data_cleaned
+)
+SELECT album_name,released_year, total_tracks, track_name as 'top track', popularity as track_popularity
+FROM ranked_track_album
+WHERE album_type = 'album' and rank_track = 1;
 
--- 3. top single and the released year
---  this ......
-SELECT DISTINCT album_name, album_type, album_popularity,  YEAR(release_date) as released_year
-FROM track_data_cleaned
-WHERE album_type = 'single'
-ORDER BY album_popularity DESC;
-
--- 4. average track popularity by released year
+-- 3. average track popularity by released year
 SELECT YEAR(release_date) AS released_year, COUNT(track_id) AS total_tracks, ROUND(AVG(popularity),2) AS avg_track_popularity
 FROM track_data_cleaned
 GROUP BY released_year
 ORDER BY released_year;
 
--- 5. top track by year
-WITH ranked_tracks AS (
+-- 4. top track by year
+WITH ranked_track_year AS (
     SELECT track_id,track_name, YEAR(release_date) AS released_year, popularity,
         ROW_NUMBER() OVER (PARTITION BY YEAR(release_date) ORDER BY popularity DESC) AS rank_track
     FROM track_data_cleaned
 )
 SELECT released_year,track_name
-FROM ranked_tracks
+FROM ranked_track_year
 WHERE rank_track = 1
 ORDER BY released_year;
 
-
--- 6. average track popularity by released year and month
-SELECT YEAR(release_date) AS released_year, MONTH(release_date) AS released_month, COUNT(track_id) AS total_tracks, ROUND(AVG(popularity),2) AS avg_track_popularity
-FROM track_data_cleaned
-GROUP BY released_year, released_month
-ORDER BY released_year, released_month;
-
--- 7. key distributions in tracks
+-- 5. key distributions in tracks
 SELECT track_key, COUNT(*) as count
 FROM track_data_cleaned
 GROUP BY track_key
 ORDER BY count DESC;
 
--- 8. key distributions in tracks of each album released each year
-SELECT YEAR(release_date) AS released_year, album_name, track_key, COUNT(*) as count
+-- 6. key distributions in tracks of each album
+SELECT album_name, track_key, COUNT(*) as count
 FROM track_data_cleaned
-GROUP BY released_year, album_name, track_key
-ORDER BY released_year, album_name,count DESC;
+GROUP BY  album_name, track_key
+ORDER BY album_name,count DESC;
 
--- 9. mode distributions in tracks
+-- 7. mode distributions in tracks
 SELECT track_mode, COUNT(*) AS count
 FROM track_data_cleaned
 GROUP BY track_mode
 ORDER BY count DESC;
 
--- 10. mode distributions in tracks of each album released each year
-SELECT YEAR(release_date) AS released_year,album_name, track_mode, COUNT(*) AS count
+-- 8. mode distributions in tracks of each album released each year
+SELECT album_name, track_mode, COUNT(*) AS count
 FROM track_data_cleaned
-GROUP BY released_year, album_name, track_mode
-ORDER BY released_year, album_name, count DESC;
+GROUP BY album_name, track_mode
+ORDER BY album_name, count DESC;
 
--- 11. time distributions in tracks
+-- 9. time distributions in tracks
 SELECT time_signature, COUNT(*) as count
 FROM track_data_cleaned
 GROUP BY time_signature
 ORDER BY count DESC;
 
--- 12. find out the only 1 song that use time signature as 3/4
+-- 10. find out the only 1 song that use time signature as 3/4
 SELECT track_name, album_name
 FROM track_data_cleaned
 WHERE time_signature = '3/4';
 
--- 13. features of each album
-#features with their own units: duration loudness, tempo
+-- 11. average value of features with their own units: duration (m) loudness (dB), tempo (BPM)
+SELECT ROUND(AVG(duration),2) AS avg_duration_sec, ROUND(AVG(loudness),2) as avg_loudness_db, ROUND(AVG(tempo),2) as avg_tempo_bpm
+FROM track_data_cleaned;
+
+-- 12. features with their own units of each album: duration (m) loudness (dB), tempo (BPM)
 SELECT album_name, COUNT(track_id) as total_tracks,
 ROUND(AVG(duration),2) AS avg_duration_sec, ROUND(AVG(loudness),2) as avg_loudness_db, ROUND(AVG(tempo),2) as avg_tempo_bpm
 FROM track_data_cleaned
 GROUP BY album_name
 ORDER BY album_name;
 
--- 14. features with value range (0-1): acousticness, energy, danceability 
+-- 13. average value of features with range (0-1): acousticness, energy, danceability 
+SELECT ROUND(AVG(acousticness),2) AS avg_acousticness, ROUND(AVG(energy),2) as avg_energy, ROUND(AVG(danceability),2) as danceability
+FROM track_data_cleaned;
+
+-- 14. features with value range (0-1) of each album: acousticness, energy, danceability 
 SELECT album_name, COUNT(track_id) as total_tracks,
 ROUND(AVG(acousticness),2) AS avg_acousticness, ROUND(AVG(energy),2) as avg_energy, ROUND(AVG(danceability),2) as danceability
 FROM track_data_cleaned
